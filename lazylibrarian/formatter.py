1f31b43d0063e2f4618f1ee349a05b4fde5adee8
Phil Borman <phil.borman@gmail.com>
2026-08-03 10:22:45 +0200

    Another place where negative years could be valid

--- lazylibrarian/formatter.py
+++ lazylibrarian/formatter.py
@@ -423,7 +423,7 @@ def date_format(datestr, formatstr="$Y-$m-$d", context='', datelang=''):
     if not datestr:
         return ''
 
-    if datestr.isdigit():  # just issue number or year
+    if check_int(datestr, 0, positive=False):  # just issue number or year, could be negative like -412 BC
         return datestr
 
     logger = logging.getLogger(__name__)
